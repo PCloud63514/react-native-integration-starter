e33c55495005f2718614c2d6de5d34047b64896f
PCloud63514 <pcloud63514@gmail.com>
2020-06-09 13:00:34 +0900

    상단바 클릭 이벤트 프로토 타입 추가

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableOpacity, View, Image, Alert } from 'react-native';
 // react-natvigation 라이브러리 에서 StackNavigator 추가하기
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,6 +8,14 @@ import TabNavigation from './TabNavigation';
 import {FontAwesome} from "@expo/vector-icons";
 
 const AppStack = createStackNavigator();
+
+const LeftOnPress = () => {
+  Alert.alert("alert title", "Left On Press");
+}
+
+const RightOnPress = () => {
+  Alert.alert("alert title", "right On Press");
+}
 
 export default function MainNavigation() {
   return(
@@ -22,16 +30,20 @@ export default function MainNavigation() {
             headerTitle: () => (
               <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
                 <Image
-                  style={{width: 200, height: 50, resizeMode:'contain'}}
+                  style={{width: 200, height: 40, resizeMode:'contain'}}
                   source={{uri: 'https://더쥬.com/web/upload/category/logo/00c4d87bfb16fa18181a100943d8d85f_uQL3jIDLCp_7_top.jpg'}}
                 />
               </View>
             ),
             headerLeft: () => (
-              <FontAwesome name='shopping-cart' size={30} style={{ paddingLeft:10}}/>
+              <TouchableOpacity onPress={LeftOnPress}>
+                <FontAwesome name='shopping-cart' size={30} style={{ paddingLeft:10}}/>
+              </TouchableOpacity>
             ),
             headerRight: () => (
-              <FontAwesome name='search' size={30} style={{ paddingRight:20 }}/>
+              <TouchableOpacity onPress={RightOnPress}>
+                <FontAwesome name='search' size={30} style={{ paddingRight:20 }}/>
+              </TouchableOpacity>
             )
           }} />
         
